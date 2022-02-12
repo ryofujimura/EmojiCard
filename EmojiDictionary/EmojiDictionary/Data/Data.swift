e089431bbcdaac956320a92566848c020025cd26
Ryo Fuj <ryo176999@gmail.com>
2022-02-11 17:45:44 -0800

    static color, emojiAPI

--- EmojiDictionary/EmojiDictionary/Data/Data.swift
+++ EmojiDictionary/EmojiDictionary/Data/Data.swift
@@ -20,4 +20,35 @@ extension Color {
     static let unicodeText = Color(red: 138/256, green: 138/256, blue: 138/256)
 }
 
+class EmojiCharacter : ObservableObject {
+    @Published var emojiCharacter: [EmojiData] = []
+    
+}
 
+class EmojiAPI : ObservableObject {
+    @Published var emojidatas: [EmojiData] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://emoji-api.com/emojis?access_key=a88b742ade10255237bc05ff3c17101da8c763cd") else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            do {
+                let emojidatas = try JSONDecoder().decode([EmojiData].self, from: data)
+                DispatchQueue.main.async {
+                    self?.emojidatas = emojidatas
+                }
+            } catch { return }
+        }
+        task.resume()
+    }
+}
+
+struct EmojiData: Hashable, Codable, Identifiable {
+    var id = UUID()
+    let slug : String
+    let character : String
+    let unicodeName : String
+    let codePoint : String
+    let group : String
+    let subGroup : String
+}
--- EmojiDictionary/EmojiDictionary/View/HomeView.swift
+++ EmojiDictionary/EmojiDictionary/View/HomeView.swift
@@ -7,22 +7,62 @@
 
 import SwiftUI
 
+let screen = UIScreen.main.bounds
+//let data = (1...250).map { "\($0)" }
+
 struct HomeView: View {
+    @State var isAnimating = false
+    @State var offset:CGFloat = 0
+    @State private var usedWords = [String]()
+    @State private var newWord = ""
+    
+    @EnvironmentObject var emojiAPI: EmojiAPI
+    let emojiCharacter = emojiAPI.emojidatas.shuffled().prefix(20)
+    
     var body: some View {
         ZStack {
-            Color.homeBackcgroundView.ignoresSafeArea()
-            VStack {
-                ForEach(1..<100, id: \.self) { n in
-                    Text("\(n)")
-                        .foregroundColor(Color.white)
+            Color.homeBackcgroundView
+            VStack (spacing: 10) {
+                ForEach(0..<10, id: \.self ) { emoji in
+                    Text("s")
+//                        .foregroundColor(Color.white)
+////                    23
+//                    Text(emoji.character)
+//                    HStack (spacing: 10){
+//                        ForEach(0..<5, id: \.self) { nnn in
+////                            50
+//                            Button(
+//                                action: {  addNewWord()  },
+//                                label: { Text(emoji.character) }
+////                                    .font(.system(size: 20)) }
+////          emojiAPI.emojidatas.shuffled().prefix(1)        Color.red              Text("🐳").font(.system(size: 20))
+//                            )
+//                        }.frame(width: 20, height: 20)
+//                    }
+//                    .offset(x: self.isAnimating ? .random(in: -300...300) : .random(in: -300...300))
+//                    .animation(Animation.linear(duration: 20).repeatForever(), value: self.isAnimating)
                 }
             }
         }
+        .onAppear{
+            self.isAnimating = true
+            emojiAPI.fetch()
+        }
+        .ignoresSafeArea()
+    }
+    
+    func addNewWord() {
+        let answer = newWord
+        guard answer.count > 0 else { return }
+
+        usedWords.append(answer)
+        newWord = ""
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(EmojiAPI())
     }
 }
